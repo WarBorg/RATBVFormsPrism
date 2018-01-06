df54f7113e077f4980440227a6ea5013a3309b67
Sergiu Cosmin <warborg@gmail.com>
2018-01-06 05:00:23 +0200

    Updated icons and launch screen

--- RATBVFormsPrism/RATBVFormsPrism/ViewModels/BusStationViewModel.cs
+++ RATBVFormsPrism/RATBVFormsPrism/ViewModels/BusStationViewModel.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-
-using Prism.Commands;
+﻿using Prism.Commands;
 using Prism.Navigation;
-using Xamarin.Forms;
-
-//using RATBVFormsPrism.Core.Interfaces;
-//using RATBVFormsPrism.Core.ViewModels;
 using RATBVFormsPrism.Models;
 using RATBVFormsPrism.Constants;
 using RATBVFormsPrism.Views;
